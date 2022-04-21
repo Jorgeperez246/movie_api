16115b7ae20d12ab8d7ecbdbbd783768209065fd
jorgeperez246 <jorgeperez246.jp@gmail.com>
2022-04-21 18:21:20 -0400

    changed port

--- index.js
+++ index.js
@@ -303,7 +303,7 @@ app.use((err, req, res, next) => {
 });
 
 // listen for requests
-const port = process.env.PORT || "mongodb://localhost/27017/myFlixDB";
+const port = process.env.PORT || 8080;
 app.listen(port, "0.0.0.0", () => {
   console.log("Listening on Port " + port);
 });
